9f4e9e838ef29dda9adf25eab5b64dbd91a4c7e4
sudarshanchoubey <sudarshan.choubey@gmail.com>
2016-02-13 18:04:26 -0800

    Added chart to plot prediction line over inputs. Created functions for charting.

--- LinearRegressionex1/src/linearRegression/LinearRegression.java
+++ LinearRegressionex1/src/linearRegression/LinearRegression.java
@@ -7,9 +7,11 @@ import java.io.InputStreamReader;
 
 import org.knowm.xchart.Chart_XY;
 import org.knowm.xchart.Series_XY.ChartXYSeriesRenderStyle;
+import org.knowm.xchart.Series_XY;
 import org.knowm.xchart.*;
 import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.internal.style.Styler.LegendPosition;
+import org.knowm.xchart.internal.style.markers.None;
 
 
 
@@ -36,6 +38,28 @@ public class LinearRegression {
 			result = result + H.get(i,0);
 		}
 		return result/(2 * m);
+	}
+	public static Chart_XY showChart(double xData[], double yData[]) {
+		Chart_XY chart = new ChartBuilder_XY().width(840).height(620).build();
+		 
+	    // Customize Chart
+	    chart.getStyler().setDefaultSeriesRenderStyle(ChartXYSeriesRenderStyle.Scatter);
+	    chart.getStyler().setChartTitleVisible(true);
+	    chart.setTitle("Profits by population");
+	    chart.setXAxisTitle("population in 10,000s");
+	    chart.setYAxisTitle("Profits in $10,000s");
+	    chart.getStyler().setLegendPosition(LegendPosition.InsideNW);
+	    chart.getStyler().setMarkerSize(16);
+	    chart.addSeries("Inputs", xData, yData);
+		new SwingWrapper(chart).displayChart();
+		return chart;
+	}
+	public static Chart_XY showPredictionChart(Chart_XY chart,double xData[], double yData[]) {
+		Series_XY series= chart.addSeries("Predictions", xData, yData);
+		series.setChartXYSeriesRenderStyle(ChartXYSeriesRenderStyle.Line);
+		series.setMarker(new None());
+		new SwingWrapper(chart).displayChart();
+		return chart;
 	}
 	public static Matrix gradientDescent(Matrix X, Matrix y, Matrix theta, double alpha, int iterations) {
 		int m = X.getRowDimension();
@@ -66,15 +90,14 @@ public class LinearRegression {
 			theta = theta.minusEquals(temp);
 			J_History.set(i,0,computeCost(X, y, theta));
 		}
-		J_History.print(6,3);
+//		J_History.print(6,3);
 		return theta;
 	}
-	public static void main (String argv[]) throws Exception{
+	public static Matrix readInputMatrix() throws Exception {
 		InputStream is = null;
 		InputStreamReader isr = null;
 		BufferedReader br = null;
 		Matrix m = null;
-		
 		is = new FileInputStream("data/ex1data1.txt");
 		isr = new InputStreamReader(is);
 		br = new BufferedReader(isr);
@@ -83,37 +106,37 @@ public class LinearRegression {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		m.print(6,3);
-		int nrow = m.getRowDimension();
-		int ncol = m.getColumnDimension();
-		Matrix X = m.getMatrix(0,nrow -1,0, 0);
-		Matrix y = m.getMatrix(0,nrow - 1, 1, 1);
+		return m;
+	}
+	public static void main (String argv[]) throws Exception{
+		Matrix matrix = readInputMatrix();
+		int nrow = matrix.getRowDimension();
+		Matrix X = matrix.getMatrix(0,nrow -1,0, 0);
+		Matrix y = matrix.getMatrix(0,nrow - 1, 1, 1);
 		double []xData = X.getColumnPackedCopy();
 		double []yData = y.getColumnPackedCopy();
-		Chart_XY chart = new ChartBuilder_XY().width(800).height(600).build();
-		 
-	    // Customize Chart
-	    chart.getStyler().setDefaultSeriesRenderStyle(ChartXYSeriesRenderStyle.Scatter);
-	    chart.getStyler().setChartTitleVisible(false);
-	    chart.getStyler().setLegendPosition(LegendPosition.InsideSW);
-	    chart.getStyler().setMarkerSize(16);
-	    chart.addSeries("Scattered", xData, yData);
-		new SwingWrapper(chart).displayChart();
-		X.print(6,3);
-		y.print(6,3);
+		Chart_XY chart = showChart(xData, yData);
 		double[][] augX = new double[nrow][2];
 		for (int i = 0; i < nrow; i++) {
 			augX[i][0] = 1;
 			augX[i][1] = X.get(i,0);
 		}
 		X = new Matrix(augX);
-		X.print(6,3);
 		Matrix theta = new Matrix(2, 1);
-		theta.print(6,3);
 		double cost = computeCost(X, y, theta);
+		System.out.println("Starting Cost is " + Double.toString(cost));
 		int iterations = 1500;
 		double alpha = 0.01;
 		theta = gradientDescent(X, y, theta, alpha, iterations);
+		double[] prediction = null;
+		double[] inp_for_chart = null;
+		Matrix P1 = X.times(theta);
+		prediction = P1.getColumnPackedCopy();
+		P1 = X.getMatrix(0, nrow -1, 1, 1);
+		inp_for_chart = P1.getColumnPackedCopy();
+		chart = showPredictionChart(chart, inp_for_chart, prediction);
+		cost = computeCost(X, y, theta);
+		System.out.println("Final Cost is " + Double.toString(cost));
 		System.out.println("theta is :");
 		theta.print(6,3);
    }
